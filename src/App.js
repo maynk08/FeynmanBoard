1e4a259b8d4b7ce9c6544c1a6a5ba60c577bff49
Mayank Mohan <swaggerb666@gmail.com>
2022-12-02 11:23:33 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,39 +1,21 @@
-
-import AddItems from './Components/Add Items/AddItems';
-import Dashboard from './Components/Dashboard/Dashboard';
-import Homepage from './Components/Homepage/Homepage';
-import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import AddTopic from './components/AddTopic';
+import Dashboard from './components/Dashboard';
+import LandingPage from './components/LandingPage';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
-  return <BrowserRouter>
-   <Routes>
-
-    <Route path="/" element={<Homepage/>}/>
-
-    <Route path="/dashboard" element={<Dashboard/>}/>
-
-    <Route path="/AddItems" element={<AddItems/>}></Route>
-
-
-   </Routes>
-   {/* <Homepage/> */}
-   {/* <Dashboard/> */}
-   {/* <AddItems/> */}
-   </BrowserRouter> 
-
-// <Heading>Hello React !!</Heading>
-// <Routes>
-//   <Route path='/' element = {<Register/>}/>
-//   <Route path='/Register' element = {<Register/>}/>
-//   <Route path='/login' element = {<Login/>}/>
-//   <Route path= '/Books' element = {<Books/>}/>
-//   <Route path="/CreateBooks" element = {<CreateBooks/>}/>
-//   <Route path="/GetBooks" element = {<GetBooks/>}/>
-//   <Route path="/UpdateBooks" element = {<UpdateBooks/>}/>
-//   <Route path="/DeleteBooks" element = {<DeleteBooks/>}/>
-//   {/* <Route path='/contacts' element = {<div>Contact page</div>}/> */}
-// </Routes>
-// </BrowserRouter>
+  return (
+    <div className="App">
+      <ToastContainer />
+      <Routes>
+        <Route path='/' element={<LandingPage/>} />
+        <Route path="/dashboard" element={ <Dashboard/> } />
+        <Route path='/addTopic' element={<AddTopic/>} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
